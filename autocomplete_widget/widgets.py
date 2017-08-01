90fefdb1c9b7b59b52098770d3ca19c9693b1dba
Herbert Kruitbosch <H.T.Kruitbosch@rug.nl>
2017-08-01 16:16:40 +0200

    fixed bug

--- autocomplete_widget/widgets.py
+++ autocomplete_widget/widgets.py
@@ -1,29 +1,14 @@
-from django.forms.widgets import RendererMixin, Select
-from django.utils.html import html_safe
-from django.utils.six import python_2_unicode_compatible
-from django.template import loader
+from django.forms.widgets import Widget
 
 
-@html_safe
-@python_2_unicode_compatible
-class AutocompleteFieldRenderer(object):
-    def __init__(self, name, value, attrs, choices):
-        self.name = name
-        self.value = value
-        self.attrs = attrs
-        self.choices = choices
+class AutocompleteSelect(Widget):
+    template_name = 'autocomplete_widget/widget.html'
 
-    def __iter__(self):
-        for idx, choice in enumerate(self.choices):
-            yield self.choice_input_class(self.name, self.value, self.attrs.copy(), choice, idx)
+    def __init__(self, attrs=None, choices=()):
+        super(AutocompleteSelect, self).__init__(attrs)
+        self.choices = list(choices)
 
-    def __str__(self):
-        return self.render()
-
-    def render(self):
-        return loader.render_to_string('autocomplete_widget/widget.html', {'widget': self})
-
-
-class AutocompleteSelect(RendererMixin, Select):
-    renderer = AutocompleteFieldRenderer
-    _empty_value = ''
+    def get_context(self, name, value, attrs):
+        context = {}
+        context['widget'] = self
+        return context
